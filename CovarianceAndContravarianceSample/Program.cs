393c52a843da911c28a4eb815e7ee769e68eeb2c
MagaMiqaelyan <48835022+MagaMiqaelyan@users.noreply.github.com>
2019-08-10 17:33:24 +0400

    Add project files.

---
+++ CovarianceAndContravarianceSample/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CovarianceAndContravarianceSample
+{
+    public class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Eat");
+        }
+    }
+    public class Bird : Animal
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Fly");
+        }
+    }
+
+    internal delegate Animal ReturnAnimalDelegate();
+    internal delegate Bird ReturnBirdDelegate();
+
+    internal delegate void TakeAnimalDelegate(Animal a);
+    internal delegate void TakeBirdDelegate(Bird b);
+
+    internal interface IProcess<out T>
+    {
+        T Process();
+    }
+    public class AnimalProcessor<T> : IProcess<T>
+    {
+        public T Process()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal interface IZoo<in T>
+    {
+        void Add(T t);
+    }
+    public class Zoo<T> : IZoo<T>
+    {
+        public void Add(T t)
+        {
+
+        }
+    }
+
+    internal class Program
+    {
+        public static Bird GetBird()
+        {
+            return new Bird();
+        }
+        public static Animal GetAnimal()
+        {
+            return new Animal();
+        }
+
+        public static void Eat(Animal a)
+        {
+            a.Eat();
+        }
+        public static void Fly(Bird b)
+        {
+            b.Fly();
+        }
+
+        private static void Main(string[] args)
+        {
+            //delegates covariance
+            Animal a = new Bird();
+            ReturnAnimalDelegate d = GetBird;
+
+            //Delegates contravariance
+            TakeBirdDelegate d2 = Eat;
+            d2(new Bird());
+
+            //arrays covariance
+            Animal[] animals = new Bird[10];
+
+            //generics covariance
+            IProcess<Animal> animalProcessor = new AnimalProcessor<Animal>();
+            IProcess<Bird> birdProcessor = new AnimalProcessor<Bird>();
+
+            Animal animal = birdProcessor.Process();
+            animalProcessor = birdProcessor;
+
+            IEnumerable<Animal> animalList = new List<Bird>();
+
+            //generic contravariance
+            IZoo<Animal> animalZoo = new Zoo<Animal>();
+            animalZoo.Add(new Animal());
+            IZoo<Bird> birdZoo = new Zoo<Bird>();
+
+            birdZoo = animalZoo;
+            birdZoo.Add(new Bird());
+        }
+    }
+}
---
+++ Testing/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Threading;
+
+namespace Testing
+{
+    internal class Program
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static void ThreadMethod(object o)
+        {
+            for (int i = 0; i < (int)o; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(0);
+            }
+        }
+
+        [ThreadStatic]
+        public static int _field;
+
+        private static void function1()
+        {
+            Myclass.Message("In Function 1.");
+            function2();
+        }
+
+        private static void function2()
+        {
+            Myclass.Message("In Function 2.");
+        }
+              
+        private static void Main(string[] args)
+        {
+            //Myclass.Message("In Main function.");
+            //function1();
+            //Console.ReadKey();
+
+            //int x;
+            //int y = 5 * (x = 2);
+            //Console.WriteLine(y);
+
+            //int[][] matrix = new int[3][];
+            //for (int i = 0; i < matrix.Length; i++)
+            //{
+            //    matrix[i] = new int[3]; // Create inner array
+            //    for (int j = 0; j < matrix[i].Length; j++)
+            //        matrix[i][j] = i * 3 + j;
+            //}
+            //for (int i = 0; i < matrix.Length; i++)
+            //{
+            //    for (int j = 0; j < matrix[i].Length; j++)
+            //    {
+            //        Console.Write(matrix[i][j]);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            //int z;
+            //int x = int.MaxValue;
+            //int y = unchecked(x + 1);
+            //unchecked { z = x + 1; }
+            //Console.WriteLine(z);
+            //Console.WriteLine(y);
+            //Console.WriteLine(unchecked(int.MaxValue + 1));
+
+            //int n = 0;
+            //var up = Task.Run(() =>
+            //{
+            //    for (int i = 0; i < 1000000; i++)
+            //        n++;
+            //});
+            //for (int i = 0; i < 1000000; i++)
+            //    n--;
+            //up.Wait();
+            //Console.WriteLine(n);
+
+            //int isInUse = 15;
+            //if (Interlocked.Exchange(ref isInUse, 1) == 0)
+            //{
+            //    Console.WriteLine(isInUse);
+            //}
+            //int a=11, b=15;
+            //Interlocked.CompareExchange(ref isInUse, a, b);
+            //Console.WriteLine(isInUse);
+
+            //ConcurrentBag<int> bag = new ConcurrentBag<int>();
+            //bag.Add(42);
+            //bag.Add(21);
+            //int result;
+            //if (bag.TryTake(out result))
+            //    Console.WriteLine(result);
+            //if (bag.TryPeek(out result))
+            //    Console.WriteLine("There is a next item: {0}", result);
+
+            //BlockingCollection<string> col = new BlockingCollection<string>();
+            //Task read = Task.Run(() =>
+            //{
+            //    while (true)
+            //    {
+            //        Console.WriteLine(col.Take());
+            //    }
+            //});
+
+            //Task write = Task.Run(() =>
+            //{
+            //    while (true)
+            //    {
+            //        string s = Console.ReadLine();
+            //        if (string.IsNullOrWhiteSpace(s))
+            //        {
+            //            break;
+            //        }
+
+            //        col.Add(s);
+            //    }
+            //});
+            //write.Wait();
+
+            //Task<int[]> parent = Task.Run(() =>
+            //{
+            //    var results = new int[3];
+            //    new Task(() => results[0] = 0, TaskCreationOptions.AttachedToParent).Start();
+            //    new Task(() => results[1] = 1, TaskCreationOptions.AttachedToParent).Start();
+            //    new Task(() => results[2] = 2, TaskCreationOptions.AttachedToParent).Start();
+            //    return results;
+            //});
+            //var finalTask = parent.ContinueWith(
+            //parentTask =>
+            //{
+            //    foreach (int i in parentTask.Result)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //});
+            //finalTask.Wait();
+
+
+            //new Thread(() =>
+            //{
+            //    for (int x = 0; x < 10; x++)
+            //    {
+            //        _field++;
+            //        Console.WriteLine("Thread A: {0}", _field);
+            //    }
+            //}).Start();
+            //new Thread(() =>
+            //{
+            //    for (int x = 0; x < 10; x++)
+            //    {
+            //        _field++;
+            //        Console.WriteLine("Thread B: {0}", _field);
+            //    }
+            //}).Start();
+            //Console.ReadKey();
+
+            //Thread t = new Thread(new ThreadStart(ThreadMethod));
+            //t.IsBackground = true;
+            //t.Start();
+
+            //Thread t = new Thread(new ParameterizedThreadStart(ThreadMethod));
+            //t.Start(5);
+            //t.Join();
+
+            //bool stopped = false;
+            //Thread t = new Thread(new ThreadStart(() =>
+            //{
+            //    while (!stopped)
+            //    {
+            //        Console.WriteLine("Running...");
+            //        Thread.Sleep(1000);
+            //    }
+            //}));
+            //t.Start();
+            //Console.WriteLine("Press any key to exit");
+            //Console.ReadKey();
+            //stopped = true;
+            //t.Join();
+
+        }
+    }
+}
+
